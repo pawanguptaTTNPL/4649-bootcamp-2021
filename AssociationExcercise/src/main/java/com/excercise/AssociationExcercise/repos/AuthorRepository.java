e5ff0ae1d2ef61b9981253918ddafa363e74f864
pawanguptaTTNPL <pawan.gupta@tothenew.com>
2021-03-17 14:55:24 +0530

    Spring security oauth 2 Complete

--- AssociationExcercise/src/main/java/com/excercise/AssociationExcercise/repos/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.excercise.AssociationExcercise.repos;
-
-import com.excercise.AssociationExcercise.entities.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author,Integer> {
-
-}
